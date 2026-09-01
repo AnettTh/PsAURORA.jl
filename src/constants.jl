ab80a83870a179e3caa56b13f6695b7081d9ef0d
AnettTh <anett.thomassen@uit.no>
2026-09-01 14:42:45 +0200

    Added constants, might want to handle this differently later on

--- src/constants.jl
+++ src/constants.jl
@@ -6,3 +6,6 @@ const c₀ = 299792458.0        # speed of light in vacuum [m/s]
 const RE = 6378e3             # Earth's radius [m]
 const μ₀ = 1.26e-6            # Vacuum permeability [N/A²]
 const M  = 8.05e22            # Earths magnetic moment  [A m²]
+
+const z_ionosphere = 700e3              # Height of the ionosphere [m]
+const eV_in_J      = 1.602176634e-19    # Electron volt in Joules [J]
