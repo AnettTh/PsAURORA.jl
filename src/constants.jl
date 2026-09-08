c81b3b0267fdbc04420849d7c3c9b543afe2d10b
Anett Thomassen <anett.thomassen@uit.no>
2026-09-08 14:26:25 +0000

    Added temporary magnetic field strength at Earth

--- src/constants.jl
+++ src/constants.jl
@@ -9,3 +9,4 @@ const M  = 8.05e22            # Earths magnetic moment  [A m²]
 
 const z_ionosphere = 700e3              # Height of the ionosphere [m]
 const eV_in_J      = 1.602176634e-19    # Electron volt in Joules [J]
+const BE           = 3.11e-5
